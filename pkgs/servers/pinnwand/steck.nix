cc697598a3efe03f51e679e681104e5a92e95095
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-09-17 14:08:10 +0200

    fixup! steck: add setuptools to nativeBuildInputs

--- pkgs/servers/pinnwand/steck.nix
+++ pkgs/servers/pinnwand/steck.nix
@@ -14,7 +14,6 @@ python3Packages.buildPythonApplication rec {
   };
 
   postPatch = ''
-    cat setup.py
     substituteInPlace setup.py \
       --replace 'click>=7.0,<8.0' 'click' \
       --replace 'termcolor>=1.1.0,<2.0.0' 'termcolor'
